95f9faf34b4f6948c7c67037a9402c7abea8ec04
Atsuya Yamada <atsuyakocn@gmail.com>
2022-11-23 17:10:07 +0900

    Question 7, 8

--- src/main/java/com/example/codeInterview/questions/seven/Solution7.java
+++ src/main/java/com/example/codeInterview/questions/seven/Solution7.java
@@ -42,6 +42,34 @@ public class Solution7 {
         return stack.isEmpty() ? 1 : 0;
     }
 
+    // A is size
+    // B is where they look
+    // B が１の場合　int[1] int[2]と戦う
+    public int fish(int[] A, int[] B){
+        Deque<Integer> fishStack = new ArrayDeque<>();
+        int minusCount = 0;
+
+        for (int count = 0; count < A.length ; count++){
+            if (B[count]== 0 && count == 0){
+                // do nothing
+            }
+            if (B[count]== 1){
+                for (int secountCount = count + 1; secountCount < A.length; secountCount++){
+                    //1であれば 一個次の数の方向確認する　→　方向 0なら消す　1 ならbreak
+                    if (B[secountCount] == 0){
+                        if (A[count] > A[secountCount]){
+                            minusCount ++;
+                        }
+//                        if ()
+                    }
+                }
+            }
+
+        }
+        System.out.println(A.length - minusCount);
+        return A.length - minusCount;
+    };
+
     public int nesting(String S) {
         Deque<Character> stack = new ArrayDeque<>();
         for(int count = 0; count < S.length(); count++){
